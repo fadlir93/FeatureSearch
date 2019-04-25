e4ecc1d41fc1f29489a9fce31fe3a721913ffd76
Fadli Ramadhan <fadlir93@gmail.com>
2019-04-25 09:06:42 +0700

    Get API With Axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+
 class App  extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term)
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: process.env.REACT_APP_CLIENT_ID
+            }
+        })
+        console.log(response.data.results)
+        console.log(process.env)
     }
     render(){
         return (
